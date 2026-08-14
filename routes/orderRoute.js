be484c3c6d2afb00667513f929aa9f7ab3db7221
manmohan kumar <manmohankumar9508@gmail.com>
2026-08-15 00:52:18 +0530

    fix controller

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -4,7 +4,7 @@ import {Router} from "express";
 const router=Router();
 router.post("/createOrder",orderController.createOrder)
 router.post("/updateOrder/:orderId", orderController.updateOrder)
-router.get("/getOrdersByUserId/:orderId",orderController.getOrdersByUserId);
+router.get("/getOrdersByUserId/:userId",orderController.getOrdersByUserId);
 router.get("/getAllOrders/",orderController.getAllOrders);
 router.delete("/deleteOrderById/:orderId",orderController.deleteOrder);
 export default router;
